f31a6a0f51042ad1779a5f1929c918cff0fc4403
Andrey Adamovich <andrey@aestasit.com>
2017-06-02 04:39:35 +0300

    removed unused

--- 04.shutdown-hook/01_compile_code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it \
-           -v $PWD:/src \
-           java \
-           bash -c "javac /src/GracefulServer.java"
--- 05.cgroups-experiments/CPUCounter.java
+++
@@ -1,5 +0,0 @@
-public class CPUCounter {
-   public static void main(String[] args) {
-      System.out.println(Runtime.getRuntime().availableProcessors());
-   }
-}
